3f0ad2cae0a08129f96d3ab75c3cee4c2f012887
dannadeily <dannadeilydc@ufps.edu.co>
2021-10-11 23:21:03 -0500

    4
    Co-Authored-By: null <adrean130320@users.noreply.github.com>

--- vista/Header.php
+++ vista/Header.php
@@ -19,7 +19,7 @@
       </li>
     </ul>
     <form class="d-flex">
-        <a href="../modelo/userlogin.php"> <button class="btn btn-primary" type="button">Iniciar sesion</button> </a>
+        <a href="../modelo/userlogin.php"> <button type="button" class="btn">Iniciar sesion</button></a>
       </form>
   </div>
 </nav>
