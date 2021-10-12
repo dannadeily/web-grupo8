b8604a52f6fba555f20760855d18bb80a560442f
adrean130320 <adriancamilorf@ufps.edu.co>
2021-10-12 00:05:27 -0500

    3
    Co-Authored-By: null <adrean130320@users.noreply.github.com>

--- vista/Header.php
+++ vista/Header.php
@@ -1,26 +1,34 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <header  style="background-color: #aa1916;">
+      <nav class="navbar navbar-expand-sm  navbar-dark">
+      <div class="container-fluid">
+     <img src="img/logo.png" class="float-start" alt="ingsistemas" height="90">
 
-  <header  style="background-color: #aa1916;">
-    <nav class="navbar navbar-expand-sm  navbar-dark">
-    <div class="container-fluid">
-   <img src="img/logo.png" class="float-start" alt="ingsistemas" height="90">
-
-    <ul class="navbar-nav">
-      <li class="nav-item">
-        <a class="nav-link active" href="../index.php">Inicio</a>
-      </li>
-      <li class="nav-item">
-          <a class="nav-link active" href="#">Categorias</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link active" href="#">Noticias</a>
-      </li>
-      <li class="nav-item">
-          <a class="nav-link active" href="#">Contacto</a>
-      </li>
-    </ul>
-    <form class="d-flex">
-        <a href="../modelo/userlogin.php"> <button class="btn btn-primary" type="button">Iniciar sesion</button> </a>
-      </form>
-  </div>
-</nav>
-</header>
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" href="../index.php">Inicio</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link active" href="#">Categorias</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" href="#">Noticias</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link active" href="#">Contacto</a>
+        </li>
+      </ul>
+      <form class="d-flex">
+          <a href="../modelo/userlogin.php"> <button class="btn btn-primary" type="button">Iniciar sesion</button> </a>
+        </form>
+    </div>
+  </nav>
+  </header>
+  </body>
+</html>
