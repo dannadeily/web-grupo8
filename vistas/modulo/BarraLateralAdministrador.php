347ddd3dedf5256ec2eb90216a72d3340d72c192
dannadeily <dannadeilydc@ufps.edu.co>
2021-11-21 22:31:16 -0500

    barra lateral

---
+++ vistas/modulo/BarraLateralAdministrador.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="../css/BarraLateralAdministrador.css">
+    <title></title>
+  </head>
+  <body>
+      <div class="sidebar">
+        <div class="toggle-btn">
+          <div class="info">
+            <p>nombre del administrador</p>
+          </div>
+        </div>
+
+        <ul class="sidebar-menu ">
+          <li class="">
+            <a href="#">
+              Informacion convocatoria
+              <i class="fas fa-angle-down pull-right">
+              </i>
+            </a>
+            <ul class="treeview-menu menu-open" style="display:block;">
+              <li class="">
+
+                  <a href="#">
+                      Crear Convocatoria
+                  </a>
+              </li>
+              <li class="">
+
+                  <a href="#">
+                      Editar Convocatoria
+                  </a>
+              </li>
+              <li class="">
+
+                  <a href="#">
+                      Historial de Calificacion
+                  </a>
+              </li>
+            </ul>
+          </li>
+          <li class="">
+            <a href="#">
+              Categorias
+              <i class="fa fa-angle-left pull-right">
+
+              </i>
+            </a>
+            <ul class="treeview-menu menu open">
+              <li class="">
+                  <a href="#">
+                      Crear Categoria
+                  </a>
+              </li>
+              <li class="">
+
+                  <a href="#">
+                      Editar Categoria
+                  </a>
+              </li>
+            </ul>
+          </li>
+          <li class="treeview">
+            <a href="#">
+              Gererar Informe
+            </a>
+          </li>
+          <li class="treeview">
+            <a href="#">
+              Cambiar Contraseña
+            </a>
+          </li>
+        </ul>
+      </div>
+  </body>
+</html>
